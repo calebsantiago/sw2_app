6af7677401807d29e3a55c2cac0c1a612bb091c6
Caleb Santiago <20152339@aloe.ulima.edu.pe>
2019-07-07 20:50:33 -0500

    55th commit

--- public/javascripts/video.js
+++
@@ -1,5 +0,0 @@
-function linkVideo(video) {
-    var link = video.substr(video.indexOf('=') + 1, video.length);
-    var iframe = document.getElementById('iframe');
-    iframe.setAttribute('src', 'https://www.youtube.com/embed/' + link);
-}
